980318cd3f393331e26d3145245dde9f01b40f7b
Haldun35 <tsmnc054@gmail.com>
2023-12-06 13:54:50 +0100

    DemobellatrixHA

--- src/test/java/stepdefs/BaseSteps.java
+++ src/test/java/stepdefs/BaseSteps.java
@@ -6,31 +6,42 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import static stepdefs.Locator.*;
+
 import java.time.Duration;
 
 public class BaseSteps {
 
-   WebDriverWait wait;
-   WebDriver driver;
+    WebDriverWait wait;
+    WebDriver driver;
 
 
-   public BaseSteps(){
+    public BaseSteps() {
 
-      driver = Driver.getDriver();
-      wait = new WebDriverWait(driver,Duration.ofSeconds(10));
-   }
-
-
+        driver = Driver.getDriver();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
 
 
-   public void click(By locator){
+    public void click(By locator) {
 
 
+    }
 
 
-   }
+    public WebElement addToCart(String s) {
 
+       // By lAddToCart = By.xpath("//a[text()='Add to cart']"); bu locator -Locator Enum- classindan alinmistir
+       // String addedToCart = "//li//*[contains(text(),'%s')]"; bu String -Locator Enum- classindan alinmistir
 
+        String str = addedToCart;
+        By locator = By.xpath(String.format(str, s));
+        WebElement element = null;
 
+        if (driver.findElement(locator).getText().contains(s)) {
+            element = driver.findElement(lAddToCart);
+        }
+        return element;
+    }
 
 }
--- src/test/java/stepdefs/Locator.java
+++ src/test/java/stepdefs/Locator.java
@@ -6,7 +6,10 @@ public interface Locator {
 
     String url = "https://demos.bellatrix.solutions/";
     By lSearch = By.xpath("//input[@id='woocommerce-product-search-field-0']");
-    By lF9AddToCart = By.xpath("//a[@aria-label='Add “Falcon 9” to your cart']");
+
+    //By lF9AddToCart = By.xpath("//a[@aria-label='Add “Falcon 9” to your cart']");
+    By lAddToCart = By.xpath("//a[text()='Add to cart']");
+    String addedToCart = "//li//*[contains(text(),'%s')]";
     By lViewCart = By.xpath("//a[@title='View cart']");
     By lAssertF9 = By.xpath("//a[text()='Falcon 9']");
 
